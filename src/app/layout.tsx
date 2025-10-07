5d1fc99a25add3def025eeed70c5f5f60786bae5
Aadit <aaditgup@gmail.com>
2025-10-06 21:12:38 -0400

    added svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,18 @@
 import './globals.css';
-import Head from 'next/head';
 
 export const metadata = {
   title: "Aadit's Website",
   description: 'Personal portfolio of Aadit Gupta',
+  icons: {
+    icon: '/a.svg',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
+        {/* External CSS links can go here too */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
@@ -18,8 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         />
-        <link rel="icon" href="/a-svgrepo-com.svg" />
-      </Head>
+      </head>
       <body>{children}</body>
     </html>
   );
